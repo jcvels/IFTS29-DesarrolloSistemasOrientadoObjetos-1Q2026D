8421ad7d6dc6f192a520416827e9a586c527cc29
Jorge Pauvels <jcvels@uvcoding.com.ar>
2026-04-26 00:37:53 -0300

    update: define action for button-click event

--- week4/InterfaceUsuario/LogInForm.cs
+++ week4/InterfaceUsuario/LogInForm.cs
@@ -5,6 +5,37 @@ namespace InterfaceUsuario
         public frmIngresar()
         {
             InitializeComponent();
+            txtUsuario.Focus();
+        }
+
+        private void borrarDatos()
+        {
+            txtUsuario.Text = string.Empty;
+            txtContrasena.Text = string.Empty;
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            const string USUARIO = "Administrador";
+            const string CONTRASENA = "Admin1234";
+
+            if(txtUsuario.Text == USUARIO & txtContrasena.Text == CONTRASENA)
+            {
+                //this.Hide();
+                Form frmIngreso = new frmIngreso();
+                frmIngreso.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Los datos ingresados no son correctos.\nVerifiquelos y vuelva a intentarlo.",
+                    "Error de ingreso",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Stop
+                );
+                borrarDatos();
+                txtUsuario.Focus();
+            }
         }
     }
 }
