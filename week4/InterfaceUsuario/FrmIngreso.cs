b2947a91b2751c075b40da0c0f7604b33211121a
Jorge Pauvels <jcvels@uvcoding.com.ar>
2026-04-25 23:43:32 -0300

    add: frmIngreso from week 3 project

---
+++ week4/InterfaceUsuario/FrmIngreso.cs
@@ -0,0 +1,32 @@
+namespace InterfaceUsuario
+{
+    public partial class frmIngreso : Form
+    {
+        public frmIngreso()
+        {
+            InitializeComponent();
+            cboTipo.Items.Add("DNI");
+            cboTipo.Items.Add("Pasaporte");
+            cboTipo.Items.Add("Extranjero");
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e) // onClick -> INGRESAR
+        {
+            var postulante = new Postulante(
+                txtNombre.Text,
+                txtApellido.Text,
+                cboTipo.Text,
+                int.Parse(
+                    txtDocumento.Text
+                )
+            );
+
+            MessageBox.Show(
+                postulante.toString(),
+                "DATOS INGRESADOS",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+    }
+}
